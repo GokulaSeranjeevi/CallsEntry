82353fe3fea1a73ffdba9ffc2d4dc2f458f88ccf
jilaba <jilaba@JILNEW.corpljm.in>
2024-12-17 13:22:42 +0530

    Same again

---
+++ CallsEntry/src/main/java/com/jilaba/calls/query/DailyActivityQuery.java
@@ -0,0 +1,11 @@
+package com.jilaba.calls.query;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+
+public class DailyActivityQuery {
+
+}
